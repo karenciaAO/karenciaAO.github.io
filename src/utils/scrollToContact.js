c856b03ea277ce4c4ea20f4274ee92b90f81ade1
karenciaAO <karenaraqueo@gmail.com>
2025-07-22 22:28:52 -0500

    index y css
    sencillo solo informativo corto

--- src/utils/scrollToContact.js
+++
@@ -1,6 +0,0 @@
-export const scrollToContact = () => {
-    const contactSection = document.getElementById('connect');
-    if (contactSection) {
-        contactSection.scrollIntoView({ behavior: 'smooth' });
-    }
-};
